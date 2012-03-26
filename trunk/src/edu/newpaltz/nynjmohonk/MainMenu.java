347b872b431c58ff606c54f1948b954c395c8b0c
Isacc552@gmail.com <Isacc552@gmail.com@abcf8043-8bad-1499-35dc-1a2646a1fa50>
2012-03-26 22:21:07 +0000

    note and loc database helpers constructed
    need to fix new picture load problems
    
    need to add data to new databases
    
    need to create proxy point generation system
    
    try 2

--- trunk/src/edu/newpaltz/nynjmohonk/MainMenu.java
+++ trunk/src/edu/newpaltz/nynjmohonk/MainMenu.java
@@ -35,7 +35,7 @@ public class MainMenu extends Activity {
 	private AlertDialog mapChoice;
 	private ProgressDialog d = null;
 	private Map currentMap = null;
-	private String databaseURL = "http://iphone.squid890.webfactional.com/maps.sqlite";
+	private String databaseURL = "https://wyvern.cs.newpaltz.edu/~n02486417/map_app/maps.sqlite";
 	private String dbLocation;
 
 	/**
@@ -43,26 +43,26 @@ public class MainMenu extends Activity {
 	 * map table. Create the onClickListener for the Select Map button.
 	 */
 	public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        dbLocation = "/data/data/" + this.getApplicationContext().getPackageName() + "/databases/maps.sqlite";
-             
-        // Open map button shows the select dialog for a map
-        openMap = (Button) findViewById(R.id.launchMap);
-        openMap.setOnClickListener(new OnClickListener() {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		dbLocation = "/data/data/" + this.getApplicationContext().getPackageName() + "/databases/maps.sqlite";
+
+		// Open map button shows the select dialog for a map
+		openMap = (Button) findViewById(R.id.launchMap);
+		openMap.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				try {
 					final ArrayList<Map> results = Map.getDownloadedMaps(MainMenu.this);
 					if(results.size() > 0) {
-				        final CharSequence [] names = new CharSequence[results.size()];
-				        for(int i = 0; i < results.size(); i++) {
-				        	names[i] = results.get(i).getName();
-				        }
-				        
-				        AlertDialog.Builder builder = new AlertDialog.Builder(MainMenu.this);
-				        builder.setTitle("Select A Map");
-				        builder.setItems(names, new DialogInterface.OnClickListener() {
-							
+						final CharSequence [] names = new CharSequence[results.size()];
+						for(int i = 0; i < results.size(); i++) {
+							names[i] = results.get(i).getName();
+						}
+
+						AlertDialog.Builder builder = new AlertDialog.Builder(MainMenu.this);
+						builder.setTitle("Select A Map");
+						builder.setItems(names, new DialogInterface.OnClickListener() {
+
 							@Override
 							public void onClick(DialogInterface dialog, int item) {
 								// Show dialog symbolizing loading of image (or downloading of image)
@@ -70,214 +70,214 @@ public class MainMenu extends Activity {
 								currentMap = results.get(item);
 								Intent i = new Intent(MainMenu.this, MapViewActivity.class);
 								i.putExtra("myMap", currentMap);
-					    		d = ProgressDialog.show(MainMenu.this, "", "Loading map...");
+								d = ProgressDialog.show(MainMenu.this, "", "Loading map...");
 								MainMenu.this.startActivity(i); // start activity on main thread	
 							}
 						});
-				        mapChoice = builder.create();			
+						mapChoice = builder.create();			
 						mapChoice.show();
-			        } else {
-			        	AlertDialog.Builder builder = new AlertDialog.Builder(MainMenu.this);
+					} else {
+						AlertDialog.Builder builder = new AlertDialog.Builder(MainMenu.this);
 						builder.setMessage("No maps downloaded.\nClick \"Download Maps\" first.")
-							.setNeutralButton("OK", new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog, int id) {
-									
-								}
-							});
-						AlertDialog a = builder.create();
-						a.show();		
-			        }
-				} catch(Exception e) {
-		        	AlertDialog.Builder builder = new AlertDialog.Builder(MainMenu.this);
-					builder.setMessage("Your map database is corrupted, so a new one will be downloaded.")
 						.setNeutralButton("OK", new DialogInterface.OnClickListener() {
 							public void onClick(DialogInterface dialog, int id) {
-					    		d = ProgressDialog.show(MainMenu.this, "", "Downloading new database...");
-								Thread t = new Thread(downloadDatabase);
-								t.start();
+
 							}
 						});
+						AlertDialog a = builder.create();
+						a.show();		
+					}
+				} catch(Exception e) {
+					AlertDialog.Builder builder = new AlertDialog.Builder(MainMenu.this);
+					builder.setMessage("Your map database is corrupted, so a new one will be downloaded.")
+					.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+							d = ProgressDialog.show(MainMenu.this, "", "Downloading new database...");
+							Thread t = new Thread(downloadDatabase);
+							t.start();
+						}
+					});
 					AlertDialog a = builder.create();
 					a.show();							
 				}  
 			}
-        });
-        
-        // Close app button exits the application
-        closeApp = (Button) findViewById(R.id.exit);
-        closeApp.setOnClickListener(new OnClickListener() {
-        	public void onClick(View v) {
-        		//finish();
-        		android.os.Process.killProcess(android.os.Process.myPid());
-        	}
-        });
-        
-        // Clear data button clears out any downloaded maps and re-copies the database
-        downloadMaps = (Button)findViewById(R.id.download_maps);
-        downloadMaps.setOnClickListener(new OnClickListener() {
-        	public void onClick(View v) {
-    			MainMenu.this.startActivity(new Intent(MainMenu.this, DownloadMapsActivity.class)); // start activity on main thread	
-        	}
-        });
-        
-        // Check if the database exists
-        File f = new File(dbLocation);
-        if(!f.exists()) {
-        	// Either first run or something else is wrong - download the latest database
-    		d = ProgressDialog.show(this, "", "Map database does not yet exist.\nDownloading now...");
-    		Thread t2 = new Thread(downloadDatabase);
-    		t2.start();       	
-        } else {
-        	// Check for a new database (maybe do in background eventually?)
-    		d = ProgressDialog.show(this, "", "Checking for new database...");
-    		Thread t3 = new Thread(newDatabaseCheck);
-    		t3.start();
-        }
+		});
+
+		// Close app button exits the application
+		closeApp = (Button) findViewById(R.id.exit);
+		closeApp.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				//finish();
+				android.os.Process.killProcess(android.os.Process.myPid());
+			}
+		});
+
+		// Clear data button clears out any downloaded maps and re-copies the database
+		downloadMaps = (Button)findViewById(R.id.download_maps);
+		downloadMaps.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				MainMenu.this.startActivity(new Intent(MainMenu.this, DownloadMapsActivity.class)); // start activity on main thread	
+			}
+		});
+
+		// Check if the database exists
+		File f = new File(dbLocation);
+		if(!f.exists()) {
+			// Either first run or something else is wrong - download the latest database
+			d = ProgressDialog.show(this, "", "Map database does not yet exist.\nDownloading now...");
+			Thread t2 = new Thread(downloadDatabase);
+			t2.start();       	
+		} else {
+			// Check for a new database (maybe do in background eventually?)
+			d = ProgressDialog.show(this, "", "Checking for new database...");
+			Thread t3 = new Thread(newDatabaseCheck);
+			t3.start();
+		}
 	}
-	
+
 	/**
-     * Create the options menu at the main screen
-     * @param menu The menu we want to create
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	super.onCreateOptionsMenu(menu);
-    	MenuInflater inflater = getMenuInflater();
-    	inflater.inflate(R.menu.main_menu, menu);
-    	return true;
-    }
-    
-    /*@Override
+	 * Create the options menu at the main screen
+	 * @param menu The menu we want to create
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	/*@Override
     public void onUserLeaveHint() {
     	finish();
     }*/
-    
-    @Override
-    public void onPause() {
-    	super.onPause();
-    	if(d != null) {
-    		d.dismiss();
-    	}
-    }
-    
-    public void onResume() {
-    	super.onResume();
-    }
-    
-    public void onDestroy() {
-    	super.onDestroy();
-    	// Kill the app - completely!
-    	System.runFinalizersOnExit(true);
-    	System.exit(0);
-    	//android.os.Process.killProcess(android.os.Process.myPid());
-    }
-    
-    /**
-     * Setup the event responders for the options selected. 
-     * @param item The item that has been selected
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	switch(item.getItemId()) {
-    	case R.id.new_database:
-    		d = ProgressDialog.show(this, "", "Downloading new database");
-    		Thread t2 = new Thread(downloadDatabase);
-    		t2.start();
-    		break;
-    	case R.id.clear_cache:
-    		d = ProgressDialog.show(this, "", "Clearing data");
-    		Thread t1 = new Thread(clearCache);
-    		t1.start();
-    		break;
-    	case R.id.support:
-    		startActivity(new Intent(this, Support.class));
-    		break;
-    	case R.id.info:
-    		startActivity(new Intent(this, Information.class));
-    		break;
-    	}
-    	return true;
-    }	
-    
-    // Thread to delete downloaded maps and clear out and re-copy the database
-    private Runnable clearCache = new Runnable() {
-    	public void run() {
-    		// Delete map files
-    		File f = MainMenu.this.getFilesDir();
-    		File[] allFiles = f.listFiles(); 
-    		for(int i = 0; i < allFiles.length; i++) {
-    			allFiles[i].delete();
-    		}
-    		
-    		
-    		MainMenu.this.deleteDatabase("maps.sqlite");
-    		
-      		// Copy database again
-    		Thread t2 = new Thread(downloadDatabase);
-    		t2.start();    
 
-    		// Close progress dialog on main UI thread
-    		MainMenu.this.runOnUiThread(new Runnable() {
+	@Override
+	public void onPause() {
+		super.onPause();
+		if(d != null) {
+			d.dismiss();
+		}
+	}
+
+	public void onResume() {
+		super.onResume();
+	}
+
+	public void onDestroy() {
+		super.onDestroy();
+		// Kill the app - completely!
+		System.runFinalizersOnExit(true);
+		System.exit(0);
+		//android.os.Process.killProcess(android.os.Process.myPid());
+	}
+
+	/**
+	 * Setup the event responders for the options selected. 
+	 * @param item The item that has been selected
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case R.id.new_database:
+			d = ProgressDialog.show(this, "", "Downloading new database");
+			Thread t2 = new Thread(downloadDatabase);
+			t2.start();
+			break;
+		case R.id.clear_cache:
+			d = ProgressDialog.show(this, "", "Clearing data");
+			Thread t1 = new Thread(clearCache);
+			t1.start();
+			break;
+		case R.id.support:
+			startActivity(new Intent(this, Support.class));
+			break;
+		case R.id.info:
+			startActivity(new Intent(this, Information.class));
+			break;
+		}
+		return true;
+	}	
+
+	// Thread to delete downloaded maps and clear out and re-copy the database
+	private Runnable clearCache = new Runnable() {
+		public void run() {
+			// Delete map files
+			File f = MainMenu.this.getFilesDir();
+			File[] allFiles = f.listFiles(); 
+			for(int i = 0; i < allFiles.length; i++) {
+				allFiles[i].delete();
+			}
+
+
+			MainMenu.this.deleteDatabase("maps.sqlite");
+
+			// Copy database again
+			Thread t2 = new Thread(downloadDatabase);
+			t2.start();    
+
+			// Close progress dialog on main UI thread
+			MainMenu.this.runOnUiThread(new Runnable() {
 				public void run() {
 					d.dismiss();						
 				}
 			});
-    	}
-    };
-    
+		}
+	};
+
 	// Start a background thread that downloads the image (if needed) and loads the map and shows
 	// our MapViewActivity
-    private Runnable downloadDatabase = new Runnable() {
-    	public void run() {			
-    		MapDatabaseHelper.downloadDB(MainMenu.this);
+	private Runnable downloadDatabase = new Runnable() {
+		public void run() {			
+			MapDatabaseHelper.downloadDB(MainMenu.this);
 			while(MapDatabaseHelper.getLoadState() == 0); // wait while the image is in an unknown state
 			while(MapDatabaseHelper.getLoadState() == 3); // wait while image actually downloads...
 			switch(MapDatabaseHelper.getLoadState()) {
-				case 1:
-					// Database loaded successfully
-					MainMenu.this.runOnUiThread(new Runnable() {
-						public void run() {
-							d.dismiss();	
-						}
-					});
-					break;
-				case 2:
-					// There was some error in downloading the image
-					connectionError();
-					break;
+			case 1:
+				// Database loaded successfully
+				MainMenu.this.runOnUiThread(new Runnable() {
+					public void run() {
+						d.dismiss();	
+					}
+				});
+				break;
+			case 2:
+				// There was some error in downloading the image
+				connectionError();
+				break;
 			}
-    	}
-    	
-    	public void connectionError() {
+		}
+
+		public void connectionError() {
 			MainMenu.this.runOnUiThread(new Runnable() {
 				public void run() {
 					d.dismiss();
 					// Show an alert dialog that shows an error in downloading the map
 					AlertDialog.Builder builder = new AlertDialog.Builder(MainMenu.this);
 					builder.setMessage("There was an error while downloading the database.\nCheck your network settings and try again.")
-						.setNeutralButton("OK", new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int id) {
-								
-							}
-						});
+					.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+
+						}
+					});
 					AlertDialog a = builder.create();
 					a.show();							
 				}
 			});
-    	}
-    };    
-    
-    // Check the last modified date of the current database against the last modified date of the remote database
-    // to see if there is a new database available
-    private Runnable newDatabaseCheck = new Runnable() {
-    	public void run() {
-    		URL url = null;
-    		URLConnection conn = null;
-    		
-    		try {
+		}
+	};    
+
+	// Check the last modified date of the current database against the last modified date of the remote database
+	// to see if there is a new database available
+	private Runnable newDatabaseCheck = new Runnable() {
+		public void run() {
+			URL url = null;
+			URLConnection conn = null;
+
+			try {
 				url = new URL(databaseURL);
 				conn = url.openConnection();
-    		} catch (MalformedURLException e) {
+			} catch (MalformedURLException e) {
 				// Should not happen - URL is hardcoded.
 			} catch(IOException e) {
 				// Error connecting to our URL, so just tell the user there was error checking for new DB
@@ -289,7 +289,7 @@ public class MainMenu extends Activity {
 				connectionError();
 				return;
 			}
-			
+
 			long remoteModified = 0;
 			String lastModified = conn.getHeaderField("Last-Modified");
 			try {
@@ -310,10 +310,10 @@ public class MainMenu extends Activity {
 				MainMenu.this.runOnUiThread(new Runnable() {
 					public void run() {
 						d.dismiss();
-			        	// Either first run or something else is wrong - download the latest database
-			    		d = ProgressDialog.show(MainMenu.this, "", "New map database available.\nDownloading now...");
-			    		Thread t2 = new Thread(downloadDatabase);
-			    		t2.start();  												
+						// Either first run or something else is wrong - download the latest database
+						d = ProgressDialog.show(MainMenu.this, "", "New map database available.\nDownloading now...");
+						Thread t2 = new Thread(downloadDatabase);
+						t2.start();  												
 					}
 				});		
 				return;
@@ -323,22 +323,22 @@ public class MainMenu extends Activity {
 					public void run() { if(d != null) d.dismiss(); }
 				});
 			}
-    	}
-    	
-    	public void connectionError() {
+		}
+
+		public void connectionError() {
 			MainMenu.this.runOnUiThread(new Runnable() {
 				public void run() {
 					d.dismiss();
 					// Show an alert dialog that shows an error in downloading the map
 					AlertDialog.Builder builder = new AlertDialog.Builder(MainMenu.this);
 					builder.setMessage("There was an error while checking for a new database.\nApp will proceed as normal if database already exists.")
-						.setNeutralButton("OK", new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int id) {}
-						});
+					.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {}
+					});
 					AlertDialog a = builder.create();
 					a.show();							
 				}
 			});			  		
-    	}
-    };
+		}
+	};
 }
